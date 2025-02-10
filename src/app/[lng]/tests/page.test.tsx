9c4ddaf3554eb43b761da9b74197bfa4ab8f86a7
Oleksandr Vovk <oleksandr.vovk@onix-systems.com>
2025-02-10 19:32:43 +0200

    add test digital store

--- src/app/[lng]/tests/page.test.tsx
+++ src/app/[lng]/tests/page.test.tsx
@@ -6,6 +6,7 @@ import Page from '../page';
 
 describe('Page', () => {
   it('renders a heading', async () => {
+    // eslint-disable-next-line @typescript-eslint/await-thenable
     render(await Page({ params: { lng: 'en' } }));
     const heading = screen.getByRole('main');
     expect(heading).toBeInTheDocument();
